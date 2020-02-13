b783911f1adedabc665970b96604bc18cf7b4dea
aguilaraul <raula@live.com>
2020-02-13 14:20:24 -0800

    Lab 7
    Lab 7 utilizes loops learned in lecture.

---
+++ Labs/Lab7.cpp
@@ -0,0 +1,25 @@
+/*
+	@author	Raul Aguilar
+	@date	February 13, 2020
+	Lab 7: Loops
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			cout << "FizzBuzz" << endl;
+		} else if (i % 3 == 0) {
+			cout << "Fizz" << endl;
+		} else if (i % 5 == 0) {
+			cout << "Buzz" << endl;
+		} else {
+			cout << i << endl;
+		}
+	}
+
+	return 0;
+}
