221aea0274728f35fe31f1351d9d2006308bcaa1
aguilaraul <raula@live.com>
2020-02-20 13:26:26 -0800

    Week 5: Classwork

---
+++ Classwork/Thur_20200220.cpp
@@ -0,0 +1,15 @@
+// Arrays
+
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 20;
+
+int main() {
+	double scores[SIZE] = {};
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << scores[i] << endl;
+	}
+}
