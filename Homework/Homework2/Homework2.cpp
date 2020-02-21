8bb1234882a55f1b851361657191e417bfc73f90
aguilaraul <raula@live.com>
2020-02-20 17:16:29 -0800

    Homework 2 v1
    Homework 1 is complete. The user is asked to enter a size for the pyramid between 1 and 50. The pyramid is drawn on the console and saved to a text file.

--- Homework/Homework2/Homework2.cpp
+++ Homework/Homework2/Homework2.cpp
@@ -1,8 +1,17 @@
+/**
+	@author	Raul Aguilar
+	@date	March 5, 2020
+	Homework 2: Nested loops and file output
+*/
+
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+	ofstream outputFile;
+	outputFile.open("hw2_triangle.txt");
 	int size = 0;
 	
 	do {
@@ -15,15 +24,22 @@ int main() {
 	for(int i = 1; i < size; i++) {
 		for(int j = 0; j < i; j++) {
 			cout << "*";
+			outputFile << "*";
 		}
 		cout << endl;
+		outputFile << endl;
 	}
 	
 	// Midpoint to bottom of pyramid
 	for(int i = size; i > 0; i--) {
-		for(int j = i; j > 0; j--)	{
+		for(int j = i; j > 0; j--) {
 			cout << "*";
+			outputFile << "*";
 		}
 		cout << endl;
+		outputFile << endl;
 	}
+	
+	outputFile.close();
+	return 0;
 }
