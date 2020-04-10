d8a43150920329306c1b92d37a359f74d0c92fd8
aguilaraul <raula@live.com>
2020-04-09 23:16:50 -0700

    Labs: Lab 16
    Lab 15 and 16 done and ready

--- Labs/Lab16.cpp
+++ Labs/Lab16.cpp
@@ -1,67 +1,94 @@
 /*
-	@author	Raul Aguilar
-	@date	March 31, 2020
-	Lab 15 + 16
-*/
+ * @author  Raul Aguilar
+ * @date    April 9, 2020
+ * Lab 15 + 16: Make efficient change with coins
+ */
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include <string>
-#include <array>
 
 using namespace std;
 
-//const double quarter = 0.25;
-//const double dime = 0.10;
-//const double nickel = 0.05;
-//const double penny = 0.01;
+void makeChange(double c, int coinChange[]);
+
+const int SIZE = 4;
+const int COINS[] = { 25, 10, 5, 1 };
+const string NAMES[] = { "Quarters: ", "Dimes: ", "Nickels: ", "Pennies: " };
 
 int main() {
-	string answer = "";
-	double price = 0, tender = 0, change = 0;
-	double coin[] = { 0.25, 0.10, 0.05, 0.01 };
-	double coinChange[] = { 0.0, 0.0, 0.0, 0.0 };
+    string answer = "";
+    bool noChange = false, error = false;
+    double price = 0, amt = 0, change = 0;
+    int coinChange[] = { 0, 0, 0, 0 };
+    ofstream fout("receipt.txt");
 
-	do {
-		cout << "Enter price of item:" << endl;
-		cin >> price;
-		cout << "Enter tender amount:" << endl;
-		cin >> tender;
-		change = tender - price;
-		if (change > 0) {
-			cout << "Change: $" << change << endl;
-			for (int i = 0; i < 4; i++) {
-				cout << "# " << i << endl;
-				cout << "Change: " << change << endl;
-				cout << "Coin: " << coin[i] << endl;
-				cout << change - coin[i] << endl;
+    do {
+        cout << "Enter the price of the item:" << endl;
+        cin >> price;
+        cout << "Enter tender amount:" << endl;;
+        cin >> amt;
+        cout << endl;
+        change = amt - price;
 
-				while (change - coin[i] >= 0) {
-					change -= coin[i];
-					coinChange[i]++;
-					cout << "---" << endl;
-					cout << change << endl;
-					cout << coinChange[i] << endl << endl;
-				}
-				
-				cout << "Coins: " << coinChange[i] << endl;
-			}
+        if (change > 0) {
+            noChange = false;
+            error = false;
+            makeChange(change, coinChange);
 
-		} else if (change == 0) {
-			cout << "No change." << endl;
-		} else {
-			cout << "Not enough money inserted." << endl;
-		}
+            cout << "Change" << endl << "---" << endl;
+            for (int i = 0; i < SIZE; i++) {
+                cout << NAMES[i] << coinChange[i] << endl;
+            }
+            cout << endl;
+        }
+        else if (change == 0) {
+            noChange = true;
+            error = false;
+            cout << "No change." << endl;
+        }
+        else {
+            noChange = true;
+            error = true;
+            cout << "Error: Not enough money inserted." << endl;
+        }
 
-		cout << endl << "Coins:" << endl;
-		for (int i = 0; i < 4; i++) {
-			cout << coinChange[i] << endl;
-		}
+        // Out to file
+        if (!error) {
+            fout << "Price: $";
+            fout << setw(21) << setprecision(2) << fixed << price << endl;
+            fout << "Tender Amount: $";
+            fout << setw(13) << amt << endl << endl;
 
+            if (!noChange) {
+                fout << "Change" << endl;
+                for (int i = 0; i < SIZE; i++) {
+                    fout << setw(11) << left << NAMES[i];
+                    fout << setw(18) << right << coinChange[i] << endl;
+                }
+            }
+            else {
+                fout << "No change." << endl;
+            }
+            fout << endl << "******************************" << endl;
+        }
 
-		cout << endl << "Do you have another entry?" << endl;
-		cin.ignore();
-		getline(cin, answer);
+        cout << "Do you have another entry?" << endl;
+        cin.ignore();
+        getline(cin, answer);
+        cout << endl;
+    } while (answer[0] == 'y' || answer[0] == 'Y');
+    fout.close();
 
-	} while (answer[0] == 'y' || answer[0] == 'Y');
-	
-	cout << "Thank you for your purchase." << endl;
+    cout << "Thank you for using our service." << endl;
+    return 0;
+}
+
+void makeChange(double c, int coinChange[]) {
+    int change = (double)(c * 100.0);
+
+    for (int i = 0; i < SIZE; i++) {
+        coinChange[i] = change / COINS[i];
+        change -= coinChange[i] * COINS[i];
+    }
 }
